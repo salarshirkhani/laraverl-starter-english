45dd9f9dfb74d5fa0b597e2894c0e95e85a93d9b
salarshirkhani <salarshirkhany16@gmail.com>
2021-12-29 11:22:21 +0330

    add genres and movies genres  model

--- app/Models/serie_genres.php
+++ app/Models/serie_genres.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class serie_genres extends Model
 {
     use HasFactory;
+    
+    protected $fillable=['genre_id', 'serie_id'];
+    
+    public function genre() {
+        return $this->belongsTo('App\Models\genres', 'genre_id');
+    }
+
+    public function serie() {
+        return $this->belongsTo('App\Models\series', 'serie_id');
+    }
 }
+
