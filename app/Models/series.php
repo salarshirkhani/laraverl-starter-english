9188171a01b4194819e1fd814301f3a85ce909e6
salarshirkhani <salarshirkhany16@gmail.com>
2021-12-29 11:14:16 +0330

    add series model

--- app/Models/series.php
+++ app/Models/series.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class series extends Model
 {
     use HasFactory;
+    protected $table='series';
+    protected $fillable=['title','description','slug','year', 'status' , 'link', 
+    'trailer', 'featured', 'image', 'stars', 'iframe'];
+
+    public function episode() {
+        return $this->hasMany('App\Models\episode');
+    }
+    
 }
